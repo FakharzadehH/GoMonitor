a8d89b9a94e36c900eb7d8d62da91f50b36a65b8
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-17 22:37:13 +0330

    fix: fixed server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"github.com/FakharzadehH/GoMonitor/internal/config"
+	"github.com/FakharzadehH/GoMonitor/repository"
+	"github.com/labstack/echo/v4"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
+)
+
+func Start() error {
+	e := echo.New()
+	e.Use(echoMiddleware.Logger())
+	e.HTTPErrorHandler = ErrorHandler()
+	db, err := config.NewGORMConnection(config.GetConfig())
+	if err != nil {
+		return err
+	}
+
+	repos := repository.NewRepository(db)
+	//svcs := service.NewServices(repos)
+	//middlewares := middleware.NewMiddlewares(svcs, repos)
+	//handler := handlers.New(repos, svcs)
+	//routes(e, handler, middlewares)
+	return e.Start(":1323")
+}
+g
