80a70f3813d43f99785f4fd9c099e40cdca4201e
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-17 23:36:28 +0330

    feat: added api routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,1 +1,13 @@
 package server
+
+import (
+	"github.com/FakharzadehH/GoMonitor/internal/server/handlers"
+	"github.com/labstack/echo/v4"
+)
+
+func routes(e *echo.Echo, h *handlers.Handlers) {
+	api := e.Group("/api")
+	api.POST("/server", h.AddServer())
+	api.GET("/server", h.ShowServer())
+	api.GET("/server/all", h.IndexServers())
+}
