15a158efd6b84d9adb5caba50e3d16a4e670e1f2
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-17 23:36:51 +0330

    feat: added routes to server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"github.com/FakharzadehH/GoMonitor/internal/config"
+	"github.com/FakharzadehH/GoMonitor/internal/server/handlers"
 	"github.com/FakharzadehH/GoMonitor/repository"
+	"github.com/FakharzadehH/GoMonitor/service"
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 )
@@ -17,10 +19,8 @@ func Start() error {
 	}
 
 	repos := repository.NewRepository(db)
-	//svcs := service.NewServices(repos)
-	//middlewares := middleware.NewMiddlewares(svcs, repos)
-	//handler := handlers.New(repos, svcs)
-	//routes(e, handler, middlewares)
+	svcs := service.NewService(repos)
+	handler := handlers.New(repos, svcs)
+	routes(e, handler)
 	return e.Start(":1323")
 }
-g
