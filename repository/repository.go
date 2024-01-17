579ae42a9e328ecf66dbd5a7a61445b4d7f83866
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-17 22:33:48 +0330

    feat: added repo

---
+++ repository/repository.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"github.com/FakharzadehH/GoMonitor/internal/domain"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+
+func (r *Repository) Upsert(status *domain.ServerStatus) error {
+	return r.db.Save(status).Error
+}
+
+func (r *Repository) GetServerStatusByID(id uint, status *domain.ServerStatus) error {
+	return r.db.First(status, id).Error
+}
+
+func (r *Repository) GetServerStatusByName(address string, status *domain.ServerStatus) error {
+	return r.db.Where("address = ?", address).Find(&status).Error
+}
+
+func (r *Repository) GetAllServers() ([]domain.ServerStatus, error) {
+	servers := []domain.ServerStatus{}
+	return servers, r.db.Find(&servers).Error
+}
