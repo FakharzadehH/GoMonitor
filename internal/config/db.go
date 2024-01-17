18876b59fe6a40ec2c781107f854a2517cdb78f6
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-17 21:46:27 +0330

    fix: added ssl mode and changet getURI to GetDSN

--- internal/config/db.go
+++ internal/config/db.go
@@ -8,11 +8,12 @@ type DB struct {
 	Username string `mapstructure:"username"`
 	Password string `mapstructure:"password"`
 	DBName   string `mapstructure:"db_name"`
+	SSLMode  string `mapstructure:"ssl_mode"`
 }
 
 const DBMSPostgres = "postgres"
 
-func (db DB) GetURI() string {
-	return fmt.Sprintf("%s://%s:%s@%s:%s/%s",
-		DBMSPostgres, db.Username, db.Password, db.Host, db.Port, db.DBName)
+func (db DB) GetDSN() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		db.Host, db.Username, db.Password, db.DBName, db.Port, db.SSLMode)
 }
