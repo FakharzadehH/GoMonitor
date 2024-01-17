1ee82d3d20b58ae9288fb77f9bd2ae9fc4752dbb
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-17 23:06:58 +0330

    feat: added service.go

--- service/service.go
+++ service/service.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"github.com/FakharzadehH/GoMonitor/internal/domain"
+	"github.com/FakharzadehH/GoMonitor/internal/logger"
 	"github.com/FakharzadehH/GoMonitor/repository"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
 )
 
 type Service struct {
@@ -12,4 +16,39 @@ func NewService(Repos *repository.Repository) *Service {
 	return &Service{
 		Repos: Repos,
 	}
+}
+
+func (s *Service) AddServer(payload domain.AddServerRequest) (*domain.AddServerResponse, error) {
+	serverStatus := &domain.ServerStatus{}
+	err := s.Repos.GetServerStatusByAddress(payload.Address, serverStatus)
+	if !errors.Is(err, gorm.ErrRecordNotFound) {
+		if err != nil {
+			logger.Logger().Errorw("error while adding server", "error", err)
+			return nil, err
+		}
+		return nil, errors.New("A server with this address is already registered")
+	}
+	serverStatus.Address = payload.Address
+	if err := s.Repos.Upsert(serverStatus); err != nil {
+		return nil, err
+	}
+	return &domain.AddServerResponse{ServerID: serverStatus.ID}, nil
+}
+
+func (s *Service) GetServerByID(id uint) (*domain.StatusShowResponse, error) {
+	serverStatus := domain.ServerStatus{}
+	if err := s.Repos.GetServerStatusByID(id, &serverStatus); err != nil {
+		logger.Logger().Errorw("Error while getting server from db by id ", "error", err)
+		return nil, err
+	}
+	return &domain.StatusShowResponse{Server: serverStatus}, nil
+}
+
+func (s *Service) GetAllServers() (*domain.StatusIndexResponse, error) {
+	servers, err := s.Repos.GetAllServers()
+	if err != nil {
+		logger.Logger().Errorw("error while getting all servers", "error", err)
+		return nil, err
+	}
+	return &domain.StatusIndexResponse{Servers: servers}, nil
 }
