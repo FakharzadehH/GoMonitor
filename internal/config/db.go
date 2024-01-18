97b11cb84a036c6ff01dfa434a6fb52466b31de3
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-18 11:34:39 +0330

    fix: changed db from mono to read/write

--- internal/config/db.go
+++ internal/config/db.go
@@ -3,17 +3,26 @@ package config
 import "fmt"
 
 type DB struct {
-	Host     string `mapstructure:"host"`
-	Port     string `mapstructure:"port"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
-	DBName   string `mapstructure:"db_name"`
-	SSLMode  string `mapstructure:"ssl_mode"`
+	WriteHost     string `mapstructure:"write_host"`
+	WritePort     string `mapstructure:"write_port"`
+	WriteUsername string `mapstructure:"write_username"`
+	WritePassword string `mapstructure:"write_password"`
+	ReadHost      string `mapstructure:"read_host"`
+	ReadPort      string `mapstructure:"read_port"`
+	ReadUsername  string `mapstructure:"read_username"`
+	ReadPassword  string `mapstructure:"read_password"`
+	DBName        string `mapstructure:"db_name"`
+	SSLMode       string `mapstructure:"ssl_mode"`
 }
 
 const DBMSPostgres = "postgres"
 
-func (db DB) GetDSN() string {
+func (db DB) GetWriteDSN() string {
 	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		db.Host, db.Username, db.Password, db.DBName, db.Port, db.SSLMode)
+		db.WriteHost, db.WriteUsername, db.WritePassword, db.DBName, db.WritePort, db.SSLMode)
+}
+
+func (db DB) GetReadDSN() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		db.ReadHost, db.ReadUsername, db.ReadPassword, db.DBName, db.ReadPort, db.SSLMode)
 }
