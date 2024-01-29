700646f1f22086d54d8673fafee75e348cc11b9c
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-01-30 00:31:29 +0330

    feat: added metrics to server health job

--- internal/job/serverhealth.go
+++ internal/job/serverhealth.go
@@ -3,6 +3,7 @@ package job
 import (
 	"github.com/FakharzadehH/GoMonitor/internal/config"
 	"github.com/FakharzadehH/GoMonitor/internal/logger"
+	"github.com/FakharzadehH/GoMonitor/internal/metrics"
 	"github.com/FakharzadehH/GoMonitor/repository"
 	"github.com/go-co-op/gocron"
 	"gopkg.in/guregu/null.v4"
@@ -20,61 +21,80 @@ func CheckServersHealthJob() {
 	if err != nil {
 		logger.Logger().Fatal(err)
 	}
+	metrics := metrics.NewMetrics()
+	monitorMetrics := metrics.NewMonitorMetrics("GoMonitor")
 	writeRepo := repository.NewRepository(writeDB) // a good practice would be to have 2 separate repo implementations
 	ReadRepo := repository.NewRepository(readDB)
 	TehranTime, _ := time.LoadLocation("Asia/Tehran")
 	s := gocron.NewScheduler(TehranTime)
 	s.Every(cfg.CheckInterval).Minute().Do(func() {
-		logger.Logger().Debugw("started Check Servers Health Job")
-		//TODO: add metrics of job starts
+		monitorMetrics.JobStart.Inc()
 		startTime := time.Now()
-		checkServersHealth(writeRepo, ReadRepo)
-		duration := time.Since(startTime)
+		checkServersHealth(writeRepo, ReadRepo, monitorMetrics)
+		duration := time.Since(startTime).Milliseconds()
+		monitorMetrics.JobDuration.Add(float64(duration))
+
 		logger.Logger().Debugw("Finished Check Servers Health Job", "duration", duration)
 	})
 	s.StartAsync()
 }
 
-func checkServersHealth(writeRepo *repository.Repository, readRepo *repository.Repository) {
+func checkServersHealth(writeRepo *repository.Repository, readRepo *repository.Repository, monitorMetrics *metrics.MonitorMetrics) {
 	servers, err := readRepo.GetAllServers()
 	if err != nil {
-		logger.Logger().Errorw("error in CheckServersHealthJob", "error", err)
+		logger.Logger().Errorw("error while getting servers list in CheckServersHealthJob", "error", err)
+		monitorMetrics.DBReplyFailure.Inc()
 		return
 	}
+	httpClient := http.Client{
+		Timeout: 15 * time.Second,
+	}
 	for _, server := range servers {
-		isHealthy := sendGetRequest(server.Address)
+		isHealthy, duration := sendGetRequest(server.Address, httpClient, monitorMetrics)
 		if isHealthy {
 			server.Success += 1
-			// TODO: add success metric
+			monitorMetrics.CheckHealthStatusSuccess.Inc()
+			monitorMetrics.RequestDurationSuccess.Add(float64(duration))
 		} else {
 			server.Failure += 1
 			server.LastFailure = null.TimeFrom(time.Now())
-			// todo: add failure metric
+			monitorMetrics.CheckHealthStatusFailure.Inc()
+			monitorMetrics.RequestDurationFailure.Add(float64(duration))
+
 		}
 		if err := writeRepo.Upsert(&server); err != nil {
 			logger.Logger().Errorw("error while updating server health status", "error", err)
-			//TODO: add metric
+			monitorMetrics.DBReplyFailure.Inc()
 		}
 
 	}
 }
-func sendGetRequest(address string) bool {
-	isHealthy := false
 
-	resp, err := http.Get("http://" + address)
+func sendGetRequest(address string, httpClient http.Client, monitorMetrics *metrics.MonitorMetrics) (bool, int64) {
+	isHealthy := false
+	start := time.Now()
+	resp, err := httpClient.Get("http://" + address)
+	duration := time.Since(start)
+	monitorMetrics.SentRequest.Inc()
 	if err != nil {
-		return false
+		return false, duration.Milliseconds()
 	}
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		isHealthy = true
 	}
 
 	if resp.StatusCode >= 300 && resp.StatusCode < 400 { // if it's redirected, check https
+		start = time.Now()
 		resp, _ := http.Get("https://" + address)
+		duration = time.Since(start)
+		monitorMetrics.SentRequest.Inc()
 		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 			isHealthy = true
 		}
 	}
-	return isHealthy
+	if !isHealthy {
+		logger.Logger().Debugw("server " + address + " is not healthy")
+	}
+	return isHealthy, duration.Milliseconds()
 
 }
